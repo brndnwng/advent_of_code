632a2ad32cacaa5f4ac60bcc27c7b9a51ee1586d
Brandon Wong <bwong@sailthru.com>
2020-12-07 21:30:43 -0500

    2020 Q1

---
+++ 2020/1/aoc_1.py
@@ -0,0 +1,13 @@
+
+inputs = []
+with open("./input.txt") as f:
+    for line in f:
+        inputs.append(int(line.strip()))
+
+visited = set()
+
+for i in inputs:
+    if 2020 - i in visited:
+        print(i * (2020-i))
+    else:
+        visited.add(i)
---
+++ 2020/1/aoc_1_2.py
@@ -0,0 +1,21 @@
+inputs = []
+with open("./input.txt") as f:
+    for line in f:
+        inputs.append(int(line.strip()))
+
+
+def twoSum(nums, target):
+    visited = set()
+    for i in inputs:
+        if target - i in visited:
+            return (i * (target-i))
+        else:
+            visited.add(i)
+    return None
+
+
+for index, i in enumerate(inputs):
+    result = twoSum(inputs[index:], 2020 - i)
+    if result is not None:
+        print(i * result)
+
